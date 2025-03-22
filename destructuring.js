34a8f1d51164aaf70357bb0e64c7c319cef4d67f
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-03-23 01:47:02 +0600

    some practicee

---
+++ destructuring.js
@@ -0,0 +1,14 @@
+const friends = ['rohim', 'korim', 'josim', 'akkas'];
+const [f1, f2, f3, f4] = friends;
+// console.log(f2);
+
+const student = {
+    name: 'akas',
+    roll: 100021,
+    sFriends: ['korim', 'josim'],
+    age: 18,
+}
+
+const { age, name, sFriends, roll } = student;
+console.log(sFriends);
+
